fae782a9b2dc4589b4836c7a11867647db071472
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-11-14 03:59:13 +0300

    check travis CI

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -7,10 +7,14 @@
     Read more about conftest.py under:
     https://pytest.org/latest/plugins.html
 """
-from __future__ import print_function, absolute_import, division
+from __future__ import absolute_import, division, print_function
 
 import pytest
 
 
 def test_one():
+    return True
+
+
+def test_two():
     return True
