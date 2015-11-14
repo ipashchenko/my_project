ba6b81dabe88f79f5e09c1e68d345c3b5e4237f2
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-11-14 04:59:16 +0300

    fuck with travis).

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -20,11 +20,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
         -O miniconda.sh
     chmod +x miniconda.sh && ./miniconda.sh -b
-    pwd
     export PATH=/home/travis/miniconda2/bin:$PATH
-    echo $PATH
-    cd /home/travis/
-    ls
     conda update --yes conda
 
     # Configure the conda environment and put it in the path using the
